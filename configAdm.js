61b320b1597cb052545727c9fe4b091f2916b970
italobalm <italobalm@gmail.com>
2024-12-12 15:30:34 -0300

    Adicionando efeito na tela de configurações

--- configAdm.js
+++ configAdm.js
@@ -21,3 +21,9 @@
         });
     });
 
+  
+    function toggleSidebar() {
+        const sidebar = document.getElementById('sidebar-menu');
+        sidebar.classList.toggle('open'); // Adiciona ou remove a classe "open"
+    }
+    
